f49538ae7162e36f434a66f7823fd0bb03fedeb5
MarioFonseca26 <110215753+MarioFonseca26@users.noreply.github.com>
2026-05-24 16:35:35 -0600

    add /setup route

--- backend/routes/web.php
+++ backend/routes/web.php
@@ -5,3 +5,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/setup', function () {
+    Illuminate\Support\Facades\Artisan::call('migrate:fresh', ['--force' => true, '--seed' => true]);
+    return 'Base de datos creada y poblada exitosamente. Ya puedes volver a la app frontend e iniciar sesión con test@example.com y contraseña: password';
+});
